f1206d3641f57e67459add12ac3866bd6eef5ed7
Nawaaz Latif <Nawaaz@Nawaazs-MacBook-Air-2.local>
2018-01-26 21:34:34 +0200

    problem 1

--- waimai/settings.py
+++ waimai/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'oauth2_provider',
     'social_django',
     'rest_framework_social_oauth2',
+    'bootstrap3'
+
 ]
 
 MIDDLEWARE = [
@@ -151,3 +153,16 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
     'fields': 'id, name, email'
 }
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'waimaiapp.social_auth_pipeline.create_user_by_type',  # <--- set the path to the function
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
--- waimaiapp/admin.py
+++ waimaiapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from waimaiapp.models import Restaurant
+from waimaiapp.models import Restaurant, Customer, Driver
 admin.site.register(Restaurant)
+admin.site.register(Customer)
+admin.site.register(Driver)
